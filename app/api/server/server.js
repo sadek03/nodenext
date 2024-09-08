4644efeb997cd938c39278b0b54fb3c91570a1ed
Sadek Ali <fynmart.co@gmail.com>
2024-09-08 15:08:07 +0530

    revised node and exprees js

---
+++ app/api/server/server.js
@@ -0,0 +1,10 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// var add = function (a, b) {
+//   return a + b;
+// };
+var add = (a, b) => a + b;
+var result = add(3, 7);
+console.log(result);
